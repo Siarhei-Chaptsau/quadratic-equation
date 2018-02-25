0572fb273e4edffb6035065c012e2e872c1bf214
Siarhei <s.chaptsau@gmail.com>
2018-02-25 22:49:30 +0300

    Implemented function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = [];
+  var arrOfTemp = equation.split(' ');
+
+  var a = +arrOfTemp[0];
+  var b = +(arrOfTemp[3] + +arrOfTemp[4]);
+  var c = +(arrOfTemp[7] + +arrOfTemp[8]);
+
+  var discr = b * b - 4 * a * c;
+  x = Math.round(-1 * b + Math.sqrt(discr)) / (2 * a);
+  y = Math.round(-1 * b - Math.sqrt(discr)) / (2 * a);
+
+  arr.push(x);
+  arr.push(y);
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  return arr;
 }
